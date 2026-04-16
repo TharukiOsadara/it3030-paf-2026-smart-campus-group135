6dac500255e7ecc439e8188aade8a5f1d9ee7e2b
Tharuki Osadara <tharukiosadara@gmail.com>
2026-04-16 08:07:32 +0530

    chore: update backend/src/main/java/com/smartcampus/exception/TicketNotFoundException.java

--- backend/src/main/java/com/smartcampus/exception/TicketNotFoundException.java
+++ backend/src/main/java/com/smartcampus/exception/TicketNotFoundException.java
@@ -5,4 +5,12 @@ public class TicketNotFoundException extends RuntimeException {
     public TicketNotFoundException(String id) {
         super("Ticket not found with id: " + id);
     }
+
+    public static TicketNotFoundException withMessage(String message) {
+        return new TicketNotFoundException(message, true);
+    }
+
+    private TicketNotFoundException(String message, boolean customMessage) {
+        super(customMessage ? message : "Ticket not found with id: " + message);
+    }
 }
