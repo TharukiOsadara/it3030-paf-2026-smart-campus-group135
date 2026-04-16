1d17279ae76f08e10896c4bd7d48d3e289b24348
Tharuki Osadara <tharukiosadara@gmail.com>
2026-04-16 08:23:33 +0530

    chore(frontend): update NewticketPage.jsx

--- frontend/src/pages/NewticketPage.jsx
+++ frontend/src/pages/NewticketPage.jsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { ArrowLeft, Upload } from "lucide-react";
-import "./NewTicketPage.css";
 import { ticketService } from "../services/TicketServices";
 
 const initialForm = {
